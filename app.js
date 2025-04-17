ce161da6a7b09e4c74bf4e74bdce4d6b1f8944af
Ansh Mani Tripathi <143960008+anshmanitripathi@users.noreply.github.com>
2025-04-17 16:19:56 +0530

    Created Routes for the listings

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOverride = require("method-override");
 
 const MONGO_URL = 'mongodb://127.0.0.1:27017/homygo';
 
@@ -18,6 +20,11 @@ async function main() {
 
 }
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended : true}));
+app.use(methodOverride("_method"));
+
 
 
 
@@ -25,24 +32,78 @@ app.get("/", (req, res) => {
     res.send("Hi , i am root");
 });
 
-app.get("/testListing", async (req, res) => {
-    let sampleListing = new Listing({
-        title: "Cozy Beachfront Cottage",
-        description:
-            "Escape to this charming beachfront cottage for a relaxing getaway. Enjoy stunning ocean views and easy access to the beach.",
-        // image: {
-        //     filename: "listingimage",
-        //     url: "https://images.unsplash.com/photo-1552733407-5d5c46c3bb3b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
-        // },
-        price: 1500,
-        location: "Malibu",
-        country: "United States",
-    });
-
-    await sampleListing.save();
-    console.log("sample was save.");
-    res.send("successful");
+// index route
+app.get("/listings", async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", {allListings});
 });
+
+//new route
+app.get("/listings/new", (req, res)=>{
+    res.render("listings/new.ejs");
+})
+
+//show route
+app.get("/listings/:id", async (req, res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", {listing});
+
+});
+
+// create listings
+app.post("/listings", async (req, res)=>{
+    // let {title , description, image, price, country, location} = req.body;
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    // console.log(listing);
+    res.redirect("/listings");
+});
+
+// Edit route
+app.get("/listings/:id/edit", async (req, res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", {listing});
+});
+
+// update route
+app.put("/listings/:id", async (req, res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect(`/listings/${id}`);
+});
+
+//delete route
+app.delete("/listings/:id", async (req, res)=>{
+    let {id} = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+});
+
+
+
+
+
+// app.get("/testListing", async (req, res) => {
+//     let sampleListing = new Listing({
+//         title: "Cozy Beachfront Cottage",
+//         description:
+//             "Escape to this charming beachfront cottage for a relaxing getaway. Enjoy stunning ocean views and easy access to the beach.",
+//         // image: {
+//         //     filename: "listingimage",
+//         //     url: "https://images.unsplash.com/photo-1552733407-5d5c46c3bb3b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+//         // },
+//         price: 1500,
+//         location: "Malibu",
+//         country: "United States",
+//     });
+
+//     await sampleListing.save();
+//     console.log("sample was save.");
+//     res.send("successful");
+// });
 
 app.listen(8080, () => {
     console.log("Server is listing to port 8080");
--- models/listing.js
+++ models/listing.js
@@ -6,7 +6,7 @@ const listingSchema = new Schema({
         type : String,
         required : true, 
     },
-    discription : String,
+    description : String,
     image : {
         type : String,
         default : "https://imgs.search.brave.com/DXyrqk1lnVF4ArRrR0YcbCogVVe2eDKW_EVwONzR7B4/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly93d3cu/aXN0b2NrcGhvdG8u/Y29tL3Jlc291cmNl/cy9pbWFnZXMvRnJl/ZVBob3Rvcy9GcmVl/LVBob3RvLTc0MHg0/OTItMTc0NDkxNTMz/My5qcGc",
